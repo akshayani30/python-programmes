12da09963e1ac66c3f147b385d7b1719961ce173
akshayani30 <agithaakshayani@gmail.com>
2025-02-18 23:12:38 +0530

    advanced

--- DSA/stack.py
+++ DSA/stack.py
@@ -1,1 +1,8 @@
 stack=[]
+stack.append(2)
+stack.append('a')
+stack.append(4)
+stack.append('b')
+stack.pop() #it removes the last index value
+stack.pop(2) #give here index value 
+print(stack)
--- list/list_methods.py
+++ list/list_methods.py
@@ -6,36 +6,43 @@ list6.insert(4,5)
 print(list6)
 
 
-# #list.remove(element)   remove first occurance of element
-# list5=[2,3,4,2]
-# list5.remove(2)
-# print(list5)
+#list.remove(element)   remove first occurance of element
+list5=[2,3,4,2]
+list5.remove(2)
+print(list5)
 
 
 
 
 
-# #list.pop(index)  removes element at index
-# list8=[1,2,3,4,5]
-# list8.pop(0)
-# print(list8)
+#list.pop(index)  removes element at index
+list8=[1,2,3,4,5]
+list8.pop(0)
+print(list8)
 
 
 
-# #list.extend([el1,el2,el3,.........eln])
-# list9=[1,2,3,4]
-# list9.extend([5])
-# print(list9)
+#list.extend([el1,el2,el3,.........eln])
+list9=[1,2,3,4]
+print(id(list9))
+list9.extend([4,5])
+print(id(list9))
+print(list9)
 
 
-# #list.index(element)   returns index of first occurance
-# list10=[1,2,3,4,5]
-# print(list10.index(2))
+#list.index(element)   returns index of first occurance
+list10=[1,2,3,4,5]
+print(list10.index(2))
 
 
-# #list.count(element)
-# list11=[1,2,1,2,1]
-# print(list11.count(1))
+#list.count(element)
+list11=[1,2,1,2,1]
+print(list11.count(1))
 
+list92=[1,2,3,4]
+print(id(list92))
+list92=list92+[4,5]
+print(id(list92))
+print(list92)
 
 
